98b5cdc2b050cc791406e4e3a3eda977bb37540a
99amiqur <99amiqur@gafe.molndal.se>
2025-03-06 13:18:10 +0100

    Implementing MongoDB Repository (Development)

--- Program.cs
+++ Program.cs
@@ -1,15 +1,49 @@
+using CloudSoft.Configurations;
+using CloudSoft.Models;
 using CloudSoft.Repositories;
 using CloudSoft.Services;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<INewsletterService, NewsletterService>();
-builder.Services.AddSingleton<ISubscriberRepository, InMemorySubscriberRepository>();
+//builder.Services.AddSingleton<ISubscriberRepository, InMemorySubscriberRepository>();
+// Check if MongoDB should be used (default to false if not specified)
+bool useMongoDb = builder.Configuration.GetValue<bool>("FeatureFlags:UseMongoDb");
 
+if (useMongoDb)
+{
+    // Configure MongoDB options
+    builder.Services.Configure<MongoDbOptions>(builder.Configuration.GetSection(MongoDbOptions.SectionName));
+
+    // Configure MongoDB client
+    builder.Services.AddSingleton<IMongoClient>(serviceProvider => {
+        var mongoDbOptions = builder.Configuration.GetSection(MongoDbOptions.SectionName).Get<MongoDbOptions>();
+        return new MongoClient(mongoDbOptions?.ConnectionString);
+    });
+
+    // Configure MongoDB collection
+    builder.Services.AddSingleton<IMongoCollection<Subscriber>>(serviceProvider => {
+        var mongoDbOptions = builder.Configuration.GetSection(MongoDbOptions.SectionName).Get<MongoDbOptions>();
+        var mongoClient = serviceProvider.GetRequiredService<IMongoClient>();
+        var database = mongoClient.GetDatabase(mongoDbOptions?.DatabaseName);
+        return database.GetCollection<Subscriber>(mongoDbOptions?.SubscribersCollectionName);
+    });
+
+    // Register MongoDB repository
+    builder.Services.AddSingleton<ISubscriberRepository, MongoDbSubscriberRepository>();
+
+    Console.WriteLine("Using MongoDB repository");
+}
+else
+{
+    // Register in-memory repository as fallback
+    builder.Services.AddSingleton<ISubscriberRepository, InMemorySubscriberRepository>();
+
+    Console.WriteLine("Using in-memory repository");
+}
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
